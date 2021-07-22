e6db35c9db4d202a9077c55c7fa8c55421526317
new-village <kazuki.niimura@gmail.com>
2021-07-22 23:39:04 +0000

    refactor codes

--- azure-services/cosmos-db.py
+++ azure-services/cosmos-db.py
@@ -1,4 +1,4 @@
-""" connection.py
+""" cosmos-db.py
 Azure COSMOS DB への接続とレコード取得用スクリプト
 
 注意：
@@ -17,7 +17,7 @@ import logging.config
 
 
 class cosmos():
-    def __init__(self, _container, _partition):
+    def __init__(self):
         # Create the client
         url = os.environ['ACCOUNT_URI']
         key = os.environ['ACCOUNT_KEY']
@@ -25,15 +25,15 @@ class cosmos():
 
         # Create or Get Database
         try:
-            self.database = client.create_database(os.environ['COSMOS_DB'])
+            self.database = client.create_database('cosmos_db')
         except exceptions.CosmosResourceExistsError:
-            self.database = client.get_database_client(os.environ['COSMOS_DB'])
+            self.database = client.get_database_client('cosmos_db')
 
         # Create or Get Container
         try:
-            self.container = self.database.create_container(id=_container, partition_key=PartitionKey(path=_partition))
+            self.container = self.database.create_container(id='sample', partition_key=PartitionKey(path='/_id'))
         except exceptions.CosmosResourceExistsError:
-            self.container = self.database.get_container_client(_container)
+            self.container = self.database.get_container_client('sample')
         except exceptions.CosmosHttpResponseError:
             raise
 
@@ -45,34 +45,30 @@ class cosmos():
         items = list(self.container.read_all_items())
         return items
 
-    def upsert(self, item):
-        res = self.container.upsert_item(item)
-        return res
-
-
-def main():
-    items = cosmos('items', '/_id')
-
-    # 件数の出力
-    print('Found {0} items'.format(items.count()))
-
-    # 取得したレコードのIDフィールドを出力
-    for doc in items.select_all():
-        print(doc)
-
-    # 新規レコードの追加
-    now = datetime.datetime.now()
-    rec = {
-        'id': str(int(items.select_all()[items.count() - 1].get('id')) + 1),
-        'create_dttm': now.strftime('%Y/%m/%dT%H:%M:%S.%fZ')
-    }
-    items.upsert(rec)
+    def upsert(self, items):
+        for i in items:
+            self.container.upsert_item(i)
 
 
 if __name__ == "__main__":
     # load logging configuration
     logging.config.fileConfig('./config/logging.ini')
-    logger = logging.getLogger(__name__)
+    logger = logging.getLogger()
 
     # call main function
-    main()
+    db = cosmos()
+
+    # 件数の出力
+    print('Found {0} items'.format(db.count()))
+
+    # 取得したレコードのIDフィールドを出力
+    for doc in db.select_all():
+        print(doc)
+
+    # 新規レコードの追加
+    now = datetime.datetime.now()
+    rec = {
+        'id': str(int(db.select_all()[db.count() - 1].get('id')) + 1),
+        'create_dttm': now.strftime('%Y/%m/%dT%H:%M:%S.%fZ')
+    }
+    db.upsert(rec)
