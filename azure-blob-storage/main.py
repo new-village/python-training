7a32650d3bc122309ae453a7044336c9551f3ce3
new-village <kazuki.niimura@gmail.com>
2021-11-27 06:36:57 +0000

    revise error handling

--- azure-blob-storage/main.py
+++ azure-blob-storage/main.py
@@ -3,7 +3,6 @@ This scipt upload file to Azure blob storage.
 """
 import logging
 import os
-import sys
 
 from azure.common import AzureHttpError, AzureMissingResourceHttpError
 from azure.core.exceptions import ResourceExistsError
@@ -24,17 +23,17 @@ if __name__ == "__main__":
         account = os.environ['STORAGE_ACCOUNT']
         key = os.environ['STORAGE_KEY1']
         service = BlockBlobService(account_name=account, account_key=key)
-    except KeyError:
+    except KeyError as e:
         logger.error('There is no expected environment variables.')
-        sys.exit('Error')
+        raise SystemExit(e)
 
     # Create Container
     try:
         service.create_container(container_name)
         logger.info('Create Container is successfully finished: ' + container_name)
-    except AzureHttpError:
+    except AzureHttpError as e:
         logger.error('The container_name should be contain lower letters: ' + container_name)
-        sys.exit('Error')
+        raise SystemExit(e)
     except ResourceExistsError:
         logger.info(container_name + ' is already exists.')
         pass
@@ -47,17 +46,22 @@ if __name__ == "__main__":
     logger.info('Upload file is successfully finished: ' + blob_name)
 
     # Get blob data from Azure blob storage
-    blob = service.get_blob_to_bytes(container_name, blob_name)
-    logger.info('Download file is successfully finished: ' + blob.content.decode())
+    try:
+        blob = service.get_blob_to_bytes(container_name, blob_name)
+        logger.info('Download file is successfully finished: ' + blob.content.decode())
+    except AzureMissingResourceHttpError:
+        logger.warning(blob_name + ' can not downloaded. File not found.')
+        pass
 
     # Delete file
     try:
         service.delete_blob(container_name, blob_name)
         logger.info('Delete file is successfully finished: ' + blob_name)
     except AzureMissingResourceHttpError:
-        logger.warning('There is no target file: ' + blob_name)
+        logger.warning(blob_name + ' can not deleted. File not found.')
         pass
 
     # Delete container
+    # There is no error occured, if there is no target container.
     service.delete_container(container_name)
     logger.info('Delete container is successfully finished: ' + container_name)
